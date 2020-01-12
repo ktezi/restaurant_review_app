b7b17ad474454c7aadddc14cc2607fe27bdf9b5d
=ktezi <=>
2020-01-12 13:42:09 +0530

    Change DB path for githu

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,8 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const host = 'https://ktezi.github.io/restaurant_review_app' //for git hub //
+    return `${host}/data/restaurants.json`;
   }
 
   /**
